1d8c665d6fba2b9b2fc90fcd8c5e83bf730b112c
BenTheSaucePot <bcrabbid@gmail.com>
2020-05-08 19:15:52 +0100

    Add project files.

---
+++ UtilMonitor/GetVals.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Management;
+using OpenHardwareMonitor.Hardware;
+public class Getter
+{
+    //These two use WMI for getting info such as CPU util and RAM avaliability
+    protected PerformanceCounter cpuCounter;
+    protected PerformanceCounter ramCounter;
+    //This uses open source software Open Hardware Monitor
+    protected Computer myComputer;
+
+    private string cpuUtil;
+    private string gpuTemp;
+
+    public Getter()
+    {
+        cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        cpuUtil = null;
+        gpuTemp = null;
+
+    }
+    public void update()
+    {
+        //Updates each value for the getters
+        cpuUtil = cpuCounter.NextValue() + "%";
+        // Needs a new contructor each time to get the values
+        myComputer = new Computer();
+        myComputer.GPUEnabled = true;
+        myComputer.Open();
+        foreach (var hardwareItem in myComputer.Hardware)
+        {
+            if (hardwareItem.HardwareType == HardwareType.GpuNvidia)
+            {
+                foreach (var sensor in hardwareItem.Sensors)
+                {
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        gpuTemp = sensor.Value.ToString();
+                    }
+                }
+            }
+        }
+        // Essentially destories the object
+        myComputer = null;
+    }
+
+
+
+
+   //ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+   public string getCPUUtil()
+        {
+            return cpuUtil;
+
+        }
+   public string getGPUTemp()
+    {
+        return gpuTemp;
+    }
+
+    }
+
+
+
+
---
+++ UtilMonitor/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Management;
+using System.Windows.Threading;
+
+namespace UtilMonitor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Threading.DispatcherTimer remainTimer = new System.Windows.Threading.DispatcherTimer();
+        public Getter g = new Getter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            remainTimer.Tick += timedEvent;
+            remainTimer.Interval = TimeSpan.FromSeconds(1);
+            remainTimer.Start();
+
+        }
+
+        private void timedEvent(object sender, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                
+                g.update();
+                CPUUtil.Text = g.getCPUUtil();
+                CPUTemp.Text = g.getGPUTemp();
+                
+
+            });
+
+            
+            
+
+        }
+        void Update()
+        {
+           
+                
+            
+        }
+
+    }
+
+}
+
+
